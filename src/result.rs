f5e61262c9a34a5bd656aea1184f46535d971305
MMunier <marc@geomaps.de>
2021-05-04 23:29:17 +0200

    preliminary Result-Conversion prototype

--- src/result.rs
+++ src/result.rs
@@ -1,4 +1,4 @@
-use std::fmt::Debug;
+use std::{fmt::Debug, io};
 
 use tracing::warn;
 
@@ -9,6 +9,8 @@ pub enum CheckerError {
     InternalError(&'static str),
 }
 
+pub type CheckerResult<T> = Result<T, CheckerError>;
+
 // FromUTF8Error auto into is disabled
 // impl From<FromUtf8Error> for CheckerError {
 //     fn from(e: FromUtf8Error) -> Self {
@@ -17,14 +19,49 @@ pub enum CheckerError {
 //     }
 // }
 
-pub type CheckerResult<T> = Result<T, CheckerError>;
+pub trait CheckerFromIOError {
+    fn into_checker_error(self, msg: &'static str) -> CheckerError;
+}
+
+impl CheckerFromIOError for io::Error {
+    fn into_checker_error(self, msg: &'static str) -> CheckerError {
+        match self.kind() {
+            io::ErrorKind::ConnectionRefused
+            | io::ErrorKind::ConnectionAborted
+            | io::ErrorKind::ConnectionReset => {
+                warn!("Converting io::Error to CheckerError OFFLINE -- {:?}", self);
+                CheckerError::Offline(msg)
+            }
+            io::ErrorKind::UnexpectedEof => {
+                warn!("Converting io::Error to CheckerError MUMBLE -- {:?}", self);
+                CheckerError::Mumble(msg)
+            }
+            _ => {
+                warn!(
+                    "Converting io::Error to CheckerError INTERNAL_ERROR -- {:?}",
+                    self
+                );
+                CheckerError::InternalError(msg)
+            }
+        }
+    }
+}
+
+pub trait CheckerfromIOResult<T> {
+    fn into_checker_result(self, msg: &'static str) -> CheckerResult<T>;
+}
+
+impl<T> CheckerfromIOResult<T> for io::Result<T> {
+    fn into_checker_result(self, msg: &'static str) -> CheckerResult<T> {
+        self.map_err(|e| e.into_checker_error(msg))
+    }
+}
 
 pub trait IntoCheckerError {
     fn into_mumble_error(self, msg: &'static str) -> CheckerError;
     fn into_offline_error(self, msg: &'static str) -> CheckerError;
     fn into_internal_error(self, msg: &'static str) -> CheckerError;
 }
-
 
 impl<T: Debug> IntoCheckerError for T {
     fn into_mumble_error(self, msg: &'static str) -> CheckerError {
@@ -43,31 +80,28 @@ impl<T: Debug> IntoCheckerError for T {
     }
 }
 
-pub trait IntoCheckerResult<T, E> 
-where E:Debug {
+pub trait IntoCheckerResult<T, E>
+where
+    E: IntoCheckerError,
+{
     fn into_mumble(self, msg: &'static str) -> CheckerResult<T>;
     fn into_offline(self, msg: &'static str) -> CheckerResult<T>;
     fn into_error(self, msg: &'static str) -> CheckerResult<T>;
 }
 
-impl<T,E> IntoCheckerResult<T,E> for Result<T,E> 
-where E:Debug {
+impl<T, E> IntoCheckerResult<T, E> for Result<T, E>
+where
+    E: Debug,
+{
     fn into_mumble(self, msg: &'static str) -> CheckerResult<T> {
-        self.map_err(|e| {
-            e.into_mumble_error(msg)
-        })
+        self.map_err(|e| e.into_mumble_error(msg))
     }
 
     fn into_offline(self, msg: &'static str) -> CheckerResult<T> {
-        self.map_err(|e| {
-            e.into_offline_error(msg)
-        })
+        self.map_err(|e| e.into_offline_error(msg))
     }
 
     fn into_error(self, msg: &'static str) -> CheckerResult<T> {
-        self.map_err(|e| {
-            e.into_internal_error(msg)
-        })
+        self.map_err(|e| e.into_internal_error(msg))
     }
 }
-
